644137a1b1f14c909dd3e43a666cc1a269e73a8c
nashokota <preom.cc.bd@gmail.com>
2025-09-20 23:47:58 +0600

    Calculator Feature

---
+++ New Folder/calculator.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+int multiply(int a, int b)
+{
+    return a * b;
+}
+int divide(int a, int b)
+{
+    return a / b;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << add(a,b) << endl;
+    cout << subtract(a,b) << endl;
+    cout << multiply(a,b) << endl;
+    cout << divide(a,b) << endl;
+        return 0;
+}
